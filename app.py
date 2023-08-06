a6736a5bd2be2ae661a0d1f02c8ca8c72b67218c
O-taxi <twitaxi024@gmail.com>
2023-08-07 03:12:12 +0900

    Initial commit

---
+++ app.py
@@ -0,0 +1,46 @@
+import os
+
+from flask import Flask, render_template, jsonify, send_from_directory, Markup, request
+from markdown import markdown
+
+app = Flask(__name__)
+
+def read_md(filepath) -> str:
+    with open(filepath, mode='r') as mdfile:
+        mdcontent = mdfile.read()
+
+    return Markup(markdown(mdcontent))
+
+@app.route('/')
+def index():
+    filename = './works/sample.md'  # 仮のファイル名
+    return render_template('index.html', filename=filename)
+
+@app.route('/files', methods=['GET'])
+def get_files():
+    base_dir = './works'
+    files = os.listdir(base_dir)
+    print(files)
+    return jsonify(files)
+
+@app.route('/file/<path:filename>', methods=['GET'])
+def get_file(filename):
+    base_dir = './works'
+    return send_from_directory(base_dir, filename, as_attachment=False)
+
+@app.route('/save', methods=['POST'])
+def save_file():
+    filename = request.form.get('filename')
+    content = request.form.get('content')
+    if type(filename) == str:
+        filepath = os.path.join("./works", filename)
+        with open(filepath, 'w') as file:
+            file.write(content)
+        ret = jsonify({"message": "Saved successfully!"}), 200
+    else:
+        ret = jsonify({"message": "filename broken"}), 200
+    
+    return ret
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=8000)
---
+++ static/js/process_markdown.js
@@ -0,0 +1,52 @@
+// SimpleMDEを初期化
+var simplemde = new SimpleMDE({ element: document.getElementById("editor") });
+simplemde.codemirror.on("change", function(){
+    var renderedHTML = marked(simplemde.value());
+    document.getElementById('preview').innerHTML = renderedHTML;
+  });
+document.getElementById('preview').classList.add("markdown-body")
+// 現在開いているファイル名を格納する変数
+var currentFile = "";
+// 初期内容を保存
+var lastContent = simplemde.value();
+
+// 内容の変更を確認して保存(5sごと)
+setInterval(function() {
+    var currentContent = simplemde.value();
+    if (lastContent !== currentContent) { // 変更がある場合のみ
+        $.ajax({
+            type: "POST",
+            url: "/save",
+            data: { 
+                content: currentContent, 
+                filename: currentFile
+            },
+            success: function(response) {
+                console.log("Saved successfully!");
+                lastContent = currentContent; // 現在の内容を保存
+            },
+            error: function(error) {
+                console.log("Error while saving:", error);
+            }
+        });
+    }
+}, 5000); // 5秒ごとに実行
+
+// ページロード時にファイル一覧を取得し、サイドバーに表示
+$(document).ready(function(){
+    $.getJSON('/files', function(data){
+    data.forEach(function(file){
+        $('#sidebar').append($('<div>').text(file).addClass('filename clickable'));
+    });
+
+    // ファイル名をクリックしたときに、そのファイルの内容を取得してエディタに表示
+    $('.filename').click(function(){
+        currentFile = $(this).text();
+        $.get('/file/' + currentFile, function(data){
+        simplemde.value(data);
+        lastContent = simplemde.value();
+        });
+    });
+    });
+});
+
